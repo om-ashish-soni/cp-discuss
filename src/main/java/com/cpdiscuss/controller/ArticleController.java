2200a5e55680373fde8b33b99240ceba2c327ebf
= <=>
2023-01-20 14:40:42 +0530

    like,view,difficulty voiting added

--- src/main/java/com/cpdiscuss/controller/ArticleController.java
+++ src/main/java/com/cpdiscuss/controller/ArticleController.java
@@ -8,6 +8,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
@@ -39,7 +40,9 @@ public class ArticleController{
         String articleName=article.getName();
         articleName=articleName.replace(' ','-');
         article.setName(articleName);
+        article.setViews(article.getViews()+1);
         Article existingArticle=getArticle(articleName);
+
         Map<String,Object> response=new HashMap<>();
         if(existingArticle!=null) {
             response.put("ok",(Object)false);
@@ -54,6 +57,41 @@ public class ArticleController{
 
     @GetMapping("/articles/get/{articleName}")
     public Article getArticle(@PathVariable String articleName){
-        return articleRepository.findByName(articleName);
+        Article article=articleRepository.findByName(articleName);
+        if(article==null) return article;
+        article.setViews(article.getViews()+1);
+        articleRepository.save(article);
+        return article;
     }
+    
+    @PutMapping("/articles/like/{articleName}")
+    public Map<String,Object> likeArticle(@PathVariable String articleName){
+        Article article=articleRepository.findByName(articleName);
+        article.setLikes(article.getLikes()+1);
+        articleRepository.save(article);
+        Map<String,Object> response=new HashMap<>();
+        response.put("ok",(Object)true);
+        response.put("likes",(Object)article.getLikes());
+        return response;
+    }
+
+    @PutMapping("/articles/vote-difficulty/{articleName}/{difficultyType}")
+    public Map<String,Object> voteDifficultyOfArticle(@PathVariable String articleName,@PathVariable String difficultyType){
+        Article article=articleRepository.findByName(articleName);
+        article.voteDifficultyType(difficultyType);
+        articleRepository.save(article);
+        Map<String,Object> response=new HashMap<>();
+        response.put("ok",(Object)true);
+        response.put("difficultyType",(Object)article.getDifficultyType());
+        return response;
+    }
+    // @GetMapping("/articles/delete/{articleName}")
+    // public Map<String,Object> deleteArticle(@PathVariable String articleName) {
+    //     Map<String,Object> response=new HashMap<>();
+    //     Article article=getArticle(articleName);
+    //     if(article==null) {
+    //     }
+    // }
+
+    
 }
--- src/main/java/com/cpdiscuss/model/Article.java
+++ src/main/java/com/cpdiscuss/model/Article.java
@@ -1,6 +1,9 @@
 package com.cpdiscuss.model;
 
 import java.util.Date;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
@@ -8,6 +11,7 @@ import org.springframework.data.mongodb.core.mapping.Document;
 @Document(collection = "articles")
 @Entity(name = "Article")
 public class Article {
+
     @Id
     private String articleId;
     private String name;
@@ -15,24 +19,64 @@ public class Article {
     private String creatorName;
     private Date createdAt;
     private Date updatedAt;
+    private List<String> tags;
+    private Long views;
+    private Long likes;
+    public String difficultyType;
+    private static final String[] difficultyList={
+        "easy","medium","hard","normal","expert"
+    };
+    private Map<String,Integer> difficultyTypeMap;
+    
     private void init(){
         this.createdAt=new Date();
         this.updatedAt=new Date();
+        this.views=0L;
+        this.likes=0L;
+        this.difficultyType="easy";
+        this.difficultyTypeMap=new HashMap<String,Integer>();
+        for(String difficultyType:difficultyList){
+            this.difficultyTypeMap.put(difficultyType,0);
+        }
+        
     }
     public Article(String name,String content) {
         this.name=name;
         this.content=content;
         this.init();
     }
+    public Article(String name,String content,String difficultyType) {
+        this.name=name;
+        this.content=content;
+        this.init();
+        this.setDifficultyType(difficultyType);
+    }
     public Article(){
         this.init();
     }
-    
+    public Long getViews(){
+        return this.views;
+    }
+    public void setViews(Long views){
+        this.views=views;
+    }
+    public Long getLikes(){
+        return this.likes;
+    }
+    public void setLikes(Long likes){
+        this.likes=likes;
+    }
     public String getName(){
         return this.name;
     }
     public String getContent(){
         return this.content;
+    }
+    public List<String> getTags(){
+        return this.tags;
+    }
+    public void setTags(List<String> tags){
+        this.tags=tags;
     }
     public void setContent(String content){
         this.content=content;
@@ -49,5 +93,34 @@ public class Article {
     }
     public Date getUpdatedAt(){
         return this.updatedAt;
+    }
+    public String calculateDifficulty(){
+        int mx=0;
+        String difficulty="easy";
+        for(Map.Entry<String,Integer> entry : this.difficultyTypeMap.entrySet()){
+            if(entry.getValue()>=mx){
+                mx=entry.getValue();
+                difficulty=entry.getKey();
+            }
+        }
+        return difficulty;
+    }
+    public void updateDifficultyType(){
+        this.difficultyType=this.calculateDifficulty();
+    }
+    public void voteDifficultyType(String difficulty){
+        int freq=1;
+        if(this.difficultyTypeMap.get(difficulty) != null){
+            freq+=this.difficultyTypeMap.get(difficulty);
+        }
+        this.difficultyTypeMap.put(difficulty,freq);
+        this.updateDifficultyType();
+        return;
+    }
+    public String getDifficultyType(){
+        return this.difficultyType;
+    }
+    public void setDifficultyType(String difficultyType){
+        this.voteDifficultyType(difficultyType);
     }
 }
